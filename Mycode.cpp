cbbf7d2a45dcc6112692b78d5c11c77e33669e56
runanexus00-debug <runanexus.00@gmail.com>
2026-05-20 21:46:59 +0600

    Binary and Decimal

--- Mycode.cpp
+++ Mycode.cpp
@@ -1,11 +1,24 @@
 
-
-//if else condition
-
 #include<iostream>
 using namespace std;
+int BinaryToDecimal(int decnum){
+  int ans=0, pow=1;
+  while(decnum>0){
+    int rem=decnum%2;
+    decnum=decnum/2;
+    ans+=rem*pow;
+    pow*=10;
+    
+  }
+  return ans;
+}
+
+
 int main(){ 
   
-  //test
+  int decnum =10;
+  
+    cout<<BinaryToDecimal(decnum)<<endl;
+  
   return 0;  
 }
